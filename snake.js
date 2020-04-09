17339eafa9ce90f16b633bfd47f99b9314d5fb5d
MadisonViaLux <madison.s.mckown@gmail.com>
2020-04-08 20:05:08 -0700

    Re working snake.js

--- snake.js
+++ snake.js
@@ -1,91 +1,89 @@
 // Hello
-(function(){
-    let SIZE = 500;
-    let GRID_SIZE = SIZE / 50;
-    let c = document.getElementById('c');
+(function() {
+    var SIZE = 500; // Size of the play-field in pixels
+    var GRID_SIZE = SIZE / 50;
+    var c = document.getElementById('c');
     
-    c.hight = c.width = SIZE * 2;
-    c.style.width = c.style.hight = SIZE + 'px';
-
-    let context = c.getContext('2d');
-    context.scale(2, 2);
-
-    let direction = newDirection = 1;
-    let snakeLength = 5;
-    let snake = [{x: SIZE/2, y: SIZE/2}];
-    let candy = null;
-    let end = false;
-
-    function randomOffSet(){
-        return Math.floor(Math.random() * SIZE / GRID_SIZE) * GRID_SIZE;
+    c.height = c.width = SIZE * 2; // 2x our resolution so retina screens look good
+    c.style.width = c.style.height = SIZE + 'px';
+    var context = c.getContext('2d');
+    context.scale(2, 2); // Scale our canvas for retina screens
+  
+    var direction = newDirection = 1; // -2: up, 2: down, -1: left, 1: right
+    var snakeLength = 5;
+    var snake = [{x: SIZE / 2, y: SIZE / 2}]; // Snake starts in the center
+    var candy = null;
+    var end = false;
+  
+    function randomOffset() {
+      return Math.floor(Math.random() * SIZE / GRID_SIZE) * GRID_SIZE;
     }
-
-    function StringifyCord(obj){
-        return [obj.x, obj.y].join(',');
+  
+    function stringifyCoord(obj) {
+      return [obj.x, obj.y].join(',');
     }
-
-    function tick(){
-        
-        let newHead = {x:snake[0].x, y:snake[0].y};
-
-        if (Math.abs(direction) !== Math.abs(newDirection)){
-            direction = newDirection;
-        }
-
-        let axis = Math.abs(direction) === 1 ? 'x' : 'y';
-
-        if (direction < 0){
-            newHead[axis] -= GRID_SIZE;
-        } else {
-            newHead[axis] += GRID_SIZE;
-        }
-
-        if (candy && candy.x === newHead.x && candy.y === newHead.y){
-            candy = null;
-            snakeLength += 10;
-        }
-
-        context.fillStyle = '#002b36';
-        context.fillRect(0, 0, SIZE, SIZE);
-
-        if (end){
-            context.fillStyle = '#eee8d5';
-            context.font = '40px serif';
-            context.textAlign = 'center';
-            context.fillText('Refresh to play again', SIZE / 2, SIZE / 2);
-        } else {
-            snake.unshift(newHead);
-            snake = snake.slice(0, snakeLength);
-        }
-
-        if (newHead.x < 0 || newHead.x >= SIZE || newHead.y < 0 || newHead.y >= SIZE){
-            end = true
-        }
-
-        context.fillStyle = '#268bd2';
-
-        let snakeObj = {};
-
-        for (var i = 0; i < snake.length; i++){
-            let a = snake[i];
-            context.fillRect(a.x, a.y, GRID_SIZE, GRID_SIZE);
-            if (i > 0) snakeObj[StringifyCord(a)] = true;
-        }
-
-        if (snakeObj[stringifyCoord(newHead)]) end = true;
-
-        while (!candy || snakeObj[stringifyCoord(candy)]){
-            candy = {x: randomOffset(), y: randomOffset()};
-        }
-
-        context.fillStyle = '#b58900';
-        context.fillRect(candy.x, candy.y, GRID_SIZE, GRID_SIZE);
+  
+    function tick() {
+      var newHead = {x: snake[0].x, y: snake[0].y};
+  
+      // Only change directon if the new direction is a different axis
+      if (Math.abs(direction) !== Math.abs(newDirection)) {
+        direction = newDirection;
+      }
+      var axis = Math.abs(direction) === 1 ? 'x' : 'y'; // 1, -1 are X; 2, -2 are Y
+      if (direction < 0) {
+        newHead[axis] -= GRID_SIZE; // Move left or down
+      } else {
+        newHead[axis] += GRID_SIZE; // Move right or up
+      }
+  
+      // Did we eat a candy? Detect if our head is in the same cell as the candy
+      if (candy && candy.x === newHead.x && candy.y === newHead.y) {
+        candy = null;
+        snakeLength += 20;
+      }
+  
+      context.fillStyle = '#002b36';
+      context.fillRect(0, 0, SIZE, SIZE); // Reset the play area
+      if (end) {
+        context.fillStyle = '#eee8d5';
+        context.font = '40px serif';
+        context.textAlign = 'center';
+        context.fillText('Refresh to play again', SIZE / 2, SIZE / 2);
+      } else {
+        snake.unshift(newHead); // Add the new head to the front
+        snake = snake.slice(0, snakeLength); // Enforce the snake's max length
+      }
+  
+      // Detect wall collisions
+      if (newHead.x < 0 || newHead.x >= SIZE || newHead.y < 0 || newHead.y >= SIZE) {
+        end = true;
+      }
+  
+      context.fillStyle = '#268bd2';
+      var snakeObj = {};
+      for (var i = 0; i < snake.length; i++) {
+        var a = snake[i];
+        context.fillRect(a.x, a.y, GRID_SIZE, GRID_SIZE); // Paint the snake
+        // Build a collision lookup object
+        if (i > 0) snakeObj[stringifyCoord(a)] = true;
+      }
+  
+      if (snakeObj[stringifyCoord(newHead)]) end = true; // Collided with our tail
+  
+      // Place a candy (not on the snake) if needed
+      while (!candy || snakeObj[stringifyCoord(candy)]) {
+        candy = {x: randomOffset(), y: randomOffset()};
+      }
+  
+      context.fillStyle = '#b58900';
+      context.fillRect(candy.x, candy.y, GRID_SIZE, GRID_SIZE); // Paint the candy
+    }
+  
+    window.onload = function() {
+      setInterval(tick, 100); // Kick off the game loop!
+      window.onkeydown = function(e) {
+        newDirection = {37: -1, 38: -2, 39: 1, 40: 2}[e.keyCode] || newDirection;
+      };
     };
-
-    window.onload = function(){
-        setInterval(tick, 100);
-        window.onkeydown = function(e){
-            newDirection = {37: -1, 38: -2, 39: 1, 40: 2}[e.keyCode] || newDirection;
-        };
-    };
-})();
+  })();
