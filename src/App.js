67071b3c80aedbc8385774b7e0db82634c401cc6
MadisonViaLux <madison.s.mckown@gmail.com>
2020-04-09 14:36:33 -0700

    adding food coord randomizer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import SnakeBody from './SnakeBody';
 import Food from './Food';
 
+
+
+const getRandomCoord = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2
+
+  return [x,y]
+}
+
+
 function App() {
 
 
   const state = {
-    food: [6,8],
+    food: getRandomCoord(),
     body: [
       [0,0],
       [2,0]
