016886b7c36fe06ad6d619c04a17472d3ad7944e
MadisonViaLux <madison.s.mckown@gmail.com>
2020-04-09 13:50:05 -0700

    creating the snake

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="game-area"></div>
+    <div className="game-area">
+      
+      <div className="snake-body" style={{top:0, left:0}}></div>
+      <div className="snake-body" style={{top:0, left:'2%'}}></div>
+      <div className="snake-body" style={{top:0, left:'4%'}}></div>
+
+
+
+    </div>
   );
 }
 
